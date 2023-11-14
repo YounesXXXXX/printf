64efc2ebb49515cd7cb2cf019af49df05384b7b9
FatimaEzzahraaAbbari <142620522+FatimaEzzahraaAbbari@users.noreply.github.com>
2023-11-14 23:35:16 +0100

    Create print_short_hex.c

---
+++ print_short_hex.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * x_prinhhex_x - prints a short decimal in hexadecimal
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int x_prinhhex_x(va_list arguments, char *buf, unsigned int ibuf)
+{
+	short int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = x_handl_buf_x(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (16 + 1));
+	binary = x_fill_binary_array_x(binary, int_input, isnegative, 16);
+	hexadecimal = malloc(sizeof(char) * (4 + 1));
+	hexadecimal = x_fill_hex_array_x(binary, hexadecimal, 0, 4);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = x_handl_buf_x(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count);
+}
