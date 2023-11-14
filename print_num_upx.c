b021f7d6619ec487b3a58b1fc08b2aac88a09446
FatimaEzzahraaAbbari <142620522+FatimaEzzahraaAbbari@users.noreply.github.com>
2023-11-14 23:28:36 +0100

    Create print_num_upx.c

---
+++ print_num_upx.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * x_prinnupx_x - prints number in uppercase hex
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int x_prinnupx_x(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = x_handl_buf_x(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	ibuf = x_handl_buf_x(buf, '0', ibuf);
+	ibuf = x_handl_buf_x(buf, 'X', ibuf);
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = x_fill_binary_array_x(binary, int_input, isnegative, 32);
+	hexadecimal = malloc(sizeof(char) * (8 + 1));
+	hexadecimal = x_fill_hex_array_x(binary, hexadecimal, 1, 8);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = x_handl_buf_x(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count + 2);
+}
