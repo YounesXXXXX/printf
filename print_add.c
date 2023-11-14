f0def5527c5603c4bfe8b4cd7fa33523a26be738
Younes Ait Lahsen <144718552+YounesXXXXX@users.noreply.github.com>
2023-11-14 18:01:41 +0100

    Create print_add.c

---
+++ print_add.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * x_print_add_x - prints the address of an input variable
+ * @arguments: input address.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer
+ *
+ * Return: number of chars printed.
+ */
+int x_print_add_x(va_list arguments, char *buf, unsigned int ibuf)
+{
+	void *add;
+	long int int_input;
+	int i, count, first_digit, isnegative;
+	char *hexadecimal, *binary;
+	char nill[] = "(nil)";
+
+	add = (va_arg(arguments, void *));
+	if (add == NULL)
+	{
+		for (i = 0; nill[i]; i++)
+			ibuf = x_handl_buf_x(buf, nill[i], ibuf);
+		return (5);
+	}
+	int_input = (intptr_t)add;
+	isnegative = 0;
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = x_fill_binary_array_x(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = x_fill_hex_array_x(binary, hexadecimal, 0, 16);
+	ibuf = x_handl_buf_x(buf, '0', ibuf);
+	ibuf = x_handl_buf_x(buf, 'x', ibuf);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = x_handl_buf_x(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count + 2);
+}
